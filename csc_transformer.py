69a8e9827425baf62c31734a4033248d135a9d05
owen198 <cfleu198@gmail.com>
2021-06-13 01:02:52 +0800

    by script

--- csc_transformer.py
+++ csc_transformer.py
@@ -38,7 +38,7 @@ gpus = tensorflow.test.gpu_device_name()
 
 import tensorflow as tf
 physical_devices = tf.config.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
+tf.config.experimental.set_memory_growth(physical_devices[0:1], enable=True)
 
 
 timesteps = 128
