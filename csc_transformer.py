73f6e11ec14687ab7ca9dcef5e606dfd7e6c733b
owen198 <cfleu198@gmail.com>
2021-06-13 10:43:55 +0800

    update

--- csc_transformer.py
+++ csc_transformer.py
@@ -36,6 +36,7 @@ gpu_devices = tensorflow.config.experimental.list_physical_devices('GPU')
 tensorflow.config.experimental.set_memory_growth(gpu_devices[0], True)
 gpus = tensorflow.test.gpu_device_name()
 
+# python csc_transformer.py W4662FM0605 W4662FM0606 2 4 128 64
 
 source = sys.argv[1]
 target = sys.argv[2]
@@ -44,7 +45,8 @@ timesteps = int(sys.argv[4])
 units_layer_1 = int(sys.argv[5])
 units_layer_2 = int(sys.argv[6])
 
-print('filename=', sys.argv[1]+'_'+target+'_'+epoch+'_'+timesteps+'_'+units_layer_1+'_'+units_layer_2)
+print('filename=', sys.argv[1]+'_'+target+'_'+str(epoch)+'_'+ \
+                    str(timesteps)+'_'+str(units_layer_1)+'_'+str(units_layer_2))
 
 
 n_features = 390
