c686806122bc321de8127276b355360917b4bc9b
owen198 <cfleu198@gmail.com>
2021-06-13 13:22:57 +0800

    update

--- csc_tuning.sh
+++ csc_tuning.sh
@@ -1,12 +1,15 @@
 python csc_transformer.py W4662FM0605 W4662FM0606 50 4 128 64
 python csc_transformer.py W4662FM0605 W4662FM0606 50 16 128 64
-python csc_transformer.py W4662FM0605 W4662FM0606 50 32 128 64
-python csc_transformer.py W4662FM0605 W4662FM0606 50 64 128 64
-python csc_transformer.py W4662FM0605 W4662FM0606 50 4 256 64
-python csc_transformer.py W4662FM0605 W4662FM0606 50 16 256 64
-python csc_transformer.py W4662FM0605 W4662FM0606 50 32 256 64
-python csc_transformer.py W4662FM0605 W4662FM0606 50 64 256 64
-python csc_transformer.py W4662FM0605 W4662FM0606 50 4 512 64
-python csc_transformer.py W4662FM0605 W4662FM0606 50 16 512 64
-python csc_transformer.py W4662FM0605 W4662FM0606 50 32 512 64
-python csc_transformer.py W4662FM0605 W4662FM0606 50 64 512 64
+#python csc_transformer.py W4662FM0605 W4662FM0606 50 32 128 64
+#python csc_transformer.py W4662FM0605 W4662FM0606 50 64 128 64
+#python csc_transformer.py W4662FM0605 W4662FM0606 50 4 256 64
+#python csc_transformer.py W4662FM0605 W4662FM0606 50 16 256 64
+#python csc_transformer.py W4662FM0605 W4662FM0606 50 32 256 64
+#python csc_transformer.py W4662FM0605 W4662FM0606 50 64 256 64
+#python csc_transformer.py W4662FM0605 W4662FM0606 50 4 512 64
+#python csc_transformer.py W4662FM0605 W4662FM0606 50 16 512 64
+#python csc_transformer.py W4662FM0605 W4662FM0606 50 32 512 64
+#python csc_transformer.py W4662FM0605 W4662FM0606 50 64 512 64
+
+python csc_transformer.py W4662FM0606 W4662FM0605 50 4 128 64
+python csc_transformer.py W4662FM0606 W4662FM0605 50 16 128 64
