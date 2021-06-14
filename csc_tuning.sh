9ab9873a0aeb164b2df73f6d44c8d19c3b150f0d
owen198 <cfleu198@gmail.com>
2021-06-14 11:46:58 +0800

    add tags and tuning conditions

--- csc_tuning.sh
+++ csc_tuning.sh
@@ -1,27 +1,25 @@
 
-#python csc_transformer.py W4662FM0605 W4662FM0606 50 32 128 64
-#python csc_transformer.py W4662FM0605 W4662FM0606 50 64 128 64
-#python csc_transformer.py W4662FM0605 W4662FM0606 50 4 256 64
-#python csc_transformer.py W4662FM0605 W4662FM0606 50 16 256 64
-#python csc_transformer.py W4662FM0605 W4662FM0606 50 32 256 64
-#python csc_transformer.py W4662FM0605 W4662FM0606 50 64 256 64
-#python csc_transformer.py W4662FM0605 W4662FM0606 50 4 512 64
-#python csc_transformer.py W4662FM0605 W4662FM0606 50 16 512 64
-#python csc_transformer.py W4662FM0605 W4662FM0606 50 32 512 64
-#python csc_transformer.py W4662FM0605 W4662FM0606 50 64 512 64
-
 python csc_transformer.py W4662FM0605 W4662FM0606 30 4 128 64
 python csc_transformer.py W4662FM0605 W4662FM0606 30 16 128 64
 python csc_transformer.py W4662FM0605 W4633070102 30 4 128 64
 python csc_transformer.py W4662FM0605 W4633070102 30 16 128 64
 python csc_transformer.py W4662FM0605 W4633080500 30 4 128 64
 python csc_transformer.py W4662FM0605 W4633080500 30 16 128 64
+python csc_transformer.py W4662FM0605 W4633080200 30 4 128 64
+python csc_transformer.py W4662FM0605 W4633080200 30 16 128 64
+python csc_transformer.py W4662FM0605 W4662FM0400 30 4 128 64
+python csc_transformer.py W4662FM0605 W4662FM0400 30 16 128 64
+
 python csc_transformer.py W4662FM0605 W4662FM0606 30 4 256 128
 python csc_transformer.py W4662FM0605 W4662FM0606 30 16 256 128
 python csc_transformer.py W4662FM0605 W4633070102 30 4 256 128
 python csc_transformer.py W4662FM0605 W4633070102 30 16 256 128
 python csc_transformer.py W4662FM0605 W4633080500 30 4 256 128
 python csc_transformer.py W4662FM0605 W4633080500 30 16 256 128
+python csc_transformer.py W4662FM0605 W4633080200 30 4 256 128
+python csc_transformer.py W4662FM0605 W4633080200 30 16 256 128
+python csc_transformer.py W4662FM0605 W4662FM0400 30 4 256 128
+python csc_transformer.py W4662FM0605 W4662FM0400 30 16 256 128
 
 python csc_transformer.py W4662FM0606 W4662FM0605 30 4 128 64
 python csc_transformer.py W4662FM0606 W4662FM0605 30 16 128 64
@@ -29,6 +27,10 @@ python csc_transformer.py W4662FM0606 W4633070102 30 4 128 64
 python csc_transformer.py W4662FM0606 W4633070102 30 16 128 64
 python csc_transformer.py W4662FM0606 W4633080500 30 4 128 64
 python csc_transformer.py W4662FM0606 W4633080500 30 16 128 64
+python csc_transformer.py W4662FM0606 W4633080200 30 4 128 64
+python csc_transformer.py W4662FM0606 W4633080200 30 16 128 64
+python csc_transformer.py W4662FM0606 W4662FM0400 30 4 128 64
+python csc_transformer.py W4662FM0606 W4662FM0400 30 16 128 64
 
 python csc_transformer.py W4633070102 W4662FM0605 30 4 128 64
 python csc_transformer.py W4633070102 W4662FM0605 30 16 128 64
@@ -36,6 +38,10 @@ python csc_transformer.py W4633070102 W4662FM0606 30 4 128 64
 python csc_transformer.py W4633070102 W4662FM0606 30 16 128 64
 python csc_transformer.py W4633070102 W4633080500 30 4 128 64
 python csc_transformer.py W4633070102 W4633080500 30 16 128 64
+python csc_transformer.py W4633070102 W4633080200 30 4 128 64
+python csc_transformer.py W4633070102 W4633080200 30 16 128 64
+python csc_transformer.py W4633070102 W4662FM0400 30 4 128 64
+python csc_transformer.py W4633070102 W4662FM0400 30 16 128 64
 
 python csc_transformer.py W4633080500 W4662FM0605 30 4 128 64
 python csc_transformer.py W4633080500 W4662FM0605 30 16 128 64
@@ -43,3 +49,29 @@ python csc_transformer.py W4633080500 W4662FM0606 30 4 128 64
 python csc_transformer.py W4633080500 W4662FM0606 30 16 128 64
 python csc_transformer.py W4633080500 W4633070102 30 4 128 64
 python csc_transformer.py W4633080500 W4633070102 30 16 128 64
+python csc_transformer.py W4633080500 W4633080200 30 4 128 64
+python csc_transformer.py W4633080500 W4633080200 30 16 128 64
+python csc_transformer.py W4633080500 W4662FM0400 30 4 128 64
+python csc_transformer.py W4633080500 W4662FM0400 30 16 128 64
+
+python csc_transformer.py W4633080200 W4662FM0605 30 4 128 64
+python csc_transformer.py W4633080200 W4662FM0605 30 16 128 64
+python csc_transformer.py W4633080200 W4662FM0606 30 4 128 64
+python csc_transformer.py W4633080200 W4662FM0606 30 16 128 64
+python csc_transformer.py W4633080200 W4633070102 30 4 128 64
+python csc_transformer.py W4633080200 W4633070102 30 16 128 64
+python csc_transformer.py W4633080200 W4633080500 30 4 128 64
+python csc_transformer.py W4633080200 W4633080500 30 16 128 64
+python csc_transformer.py W4633080200 W4662FM0400 30 4 128 64
+python csc_transformer.py W4633080200 W4662FM0400 30 16 128 64
+
+python csc_transformer.py W4662FM0400 W4662FM0605 30 4 128 64
+python csc_transformer.py W4662FM0400 W4662FM0605 30 16 128 64
+python csc_transformer.py W4662FM0400 W4662FM0606 30 4 128 64
+python csc_transformer.py W4662FM0400 W4662FM0606 30 16 128 64
+python csc_transformer.py W4662FM0400 W4633070102 30 4 128 64
+python csc_transformer.py W4662FM0400 W4633070102 30 16 128 64
+python csc_transformer.py W4662FM0400 W4633080200 30 4 128 64
+python csc_transformer.py W4662FM0400 W4633080200 30 16 128 64
+python csc_transformer.py W4662FM0400 W4633080500 30 4 128 64
+python csc_transformer.py W4662FM0400 W4633080500 30 16 128 64
---
+++ csc_tuning_1.sh
@@ -0,0 +1,77 @@
+
+python csc_transformer.py W4662FM0605 W4662FM0606 100 4 128 64
+python csc_transformer.py W4662FM0605 W4662FM0606 100 16 128 64
+python csc_transformer.py W4662FM0605 W4633070102 100 4 128 64
+python csc_transformer.py W4662FM0605 W4633070102 100 16 128 64
+python csc_transformer.py W4662FM0605 W4633080500 100 4 128 64
+python csc_transformer.py W4662FM0605 W4633080500 100 16 128 64
+python csc_transformer.py W4662FM0605 W4633080200 100 4 128 64
+python csc_transformer.py W4662FM0605 W4633080200 100 16 128 64
+python csc_transformer.py W4662FM0605 W4662FM0400 100 4 128 64
+python csc_transformer.py W4662FM0605 W4662FM0400 100 16 128 64
+
+python csc_transformer.py W4662FM0605 W4662FM0606 100 4 256 128
+python csc_transformer.py W4662FM0605 W4662FM0606 100 16 256 128
+python csc_transformer.py W4662FM0605 W4633070102 100 4 256 128
+python csc_transformer.py W4662FM0605 W4633070102 100 16 256 128
+python csc_transformer.py W4662FM0605 W4633080500 100 4 256 128
+python csc_transformer.py W4662FM0605 W4633080500 100 16 256 128
+python csc_transformer.py W4662FM0605 W4633080200 100 4 256 128
+python csc_transformer.py W4662FM0605 W4633080200 100 16 256 128
+python csc_transformer.py W4662FM0605 W4662FM0400 100 4 256 128
+python csc_transformer.py W4662FM0605 W4662FM0400 100 16 256 128
+
+python csc_transformer.py W4662FM0606 W4662FM0605 100 4 128 64
+python csc_transformer.py W4662FM0606 W4662FM0605 100 16 128 64
+python csc_transformer.py W4662FM0606 W4633070102 100 4 128 64
+python csc_transformer.py W4662FM0606 W4633070102 100 16 128 64
+python csc_transformer.py W4662FM0606 W4633080500 100 4 128 64
+python csc_transformer.py W4662FM0606 W4633080500 100 16 128 64
+python csc_transformer.py W4662FM0606 W4633080200 100 4 128 64
+python csc_transformer.py W4662FM0606 W4633080200 100 16 128 64
+python csc_transformer.py W4662FM0606 W4662FM0400 100 4 128 64
+python csc_transformer.py W4662FM0606 W4662FM0400 100 16 128 64
+
+python csc_transformer.py W4633070102 W4662FM0605 100 4 128 64
+python csc_transformer.py W4633070102 W4662FM0605 100 16 128 64
+python csc_transformer.py W4633070102 W4662FM0606 100 4 128 64
+python csc_transformer.py W4633070102 W4662FM0606 100 16 128 64
+python csc_transformer.py W4633070102 W4633080500 100 4 128 64
+python csc_transformer.py W4633070102 W4633080500 100 16 128 64
+python csc_transformer.py W4633070102 W4633080200 100 4 128 64
+python csc_transformer.py W4633070102 W4633080200 100 16 128 64
+python csc_transformer.py W4633070102 W4662FM0400 100 4 128 64
+python csc_transformer.py W4633070102 W4662FM0400 100 16 128 64
+
+python csc_transformer.py W4633080500 W4662FM0605 100 4 128 64
+python csc_transformer.py W4633080500 W4662FM0605 100 16 128 64
+python csc_transformer.py W4633080500 W4662FM0606 100 4 128 64
+python csc_transformer.py W4633080500 W4662FM0606 100 16 128 64
+python csc_transformer.py W4633080500 W4633070102 100 4 128 64
+python csc_transformer.py W4633080500 W4633070102 100 16 128 64
+python csc_transformer.py W4633080500 W4633080200 100 4 128 64
+python csc_transformer.py W4633080500 W4633080200 100 16 128 64
+python csc_transformer.py W4633080500 W4662FM0400 100 4 128 64
+python csc_transformer.py W4633080500 W4662FM0400 100 16 128 64
+
+python csc_transformer.py W4633080200 W4662FM0605 100 4 128 64
+python csc_transformer.py W4633080200 W4662FM0605 100 16 128 64
+python csc_transformer.py W4633080200 W4662FM0606 100 4 128 64
+python csc_transformer.py W4633080200 W4662FM0606 100 16 128 64
+python csc_transformer.py W4633080200 W4633070102 100 4 128 64
+python csc_transformer.py W4633080200 W4633070102 100 16 128 64
+python csc_transformer.py W4633080200 W4633080500 100 4 128 64
+python csc_transformer.py W4633080200 W4633080500 100 16 128 64
+python csc_transformer.py W4633080200 W4662FM0400 100 4 128 64
+python csc_transformer.py W4633080200 W4662FM0400 100 16 128 64
+
+python csc_transformer.py W4662FM0400 W4662FM0605 100 4 128 64
+python csc_transformer.py W4662FM0400 W4662FM0605 100 16 128 64
+python csc_transformer.py W4662FM0400 W4662FM0606 100 4 128 64
+python csc_transformer.py W4662FM0400 W4662FM0606 100 16 128 64
+python csc_transformer.py W4662FM0400 W4633070102 100 4 128 64
+python csc_transformer.py W4662FM0400 W4633070102 100 16 128 64
+python csc_transformer.py W4662FM0400 W4633080200 100 4 128 64
+python csc_transformer.py W4662FM0400 W4633080200 100 16 128 64
+python csc_transformer.py W4662FM0400 W4633080500 100 4 128 64
+python csc_transformer.py W4662FM0400 W4633080500 100 16 128 64
