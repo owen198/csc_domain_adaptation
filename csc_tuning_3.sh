f7e70042fcec6ae9326d64f15077ac4431ac8ad5
owen198 <cfleu198@gmail.com>
2021-06-22 16:30:48 +0800

    fix source and target position

--- csc_tuning_3.sh
+++ csc_tuning_3.sh
@@ -1,17 +1,17 @@
 # W4662FM0507
-python csc_transformer.py W4662FM0507 W4662FM0605 300 2 468 32 0
-python csc_transformer.py W4662FM0507 W4633070102 300 2 468 32 0
-python csc_transformer.py W4662FM0507 W4633080500 300 2 468 32 0
-python csc_transformer.py W4662FM0507 W4633080200 300 2 468 32 0
-python csc_transformer.py W4662FM0507 W4662FM0400 300 2 468 32 0
-python csc_transformer.py W4662FM0507 W4662FM0606 300 2 468 32 0
+python csc_transformer.py W4662FM0507 W4662FM0605 300 2 468 32 1
+python csc_transformer.py W4662FM0507 W4633070102 300 2 468 32 1
+python csc_transformer.py W4662FM0507 W4633080500 300 2 468 32 1
+python csc_transformer.py W4662FM0507 W4633080200 300 2 468 32 1
+python csc_transformer.py W4662FM0507 W4662FM0400 300 2 468 32 1
+python csc_transformer.py W4662FM0507 W4662FM0606 300 2 468 32 1
 
-python csc_transformer.py W4662FM0507 W4662FM0605 300 64 468 32 0
-python csc_transformer.py W4662FM0507 W4633070102 300 64 468 32 0
-python csc_transformer.py W4662FM0507 W4633080500 300 64 468 32 0
-python csc_transformer.py W4662FM0507 W4633080200 300 64 468 32 0
-python csc_transformer.py W4662FM0507 W4662FM0400 300 64 468 32 0
-python csc_transformer.py W4662FM0507 W4662FM0606 300 64 468 32 0
+python csc_transformer.py W4662FM0507 W4662FM0605 300 64 468 32 1
+python csc_transformer.py W4662FM0507 W4633070102 300 64 468 32 1
+python csc_transformer.py W4662FM0507 W4633080500 300 64 468 32 1
+python csc_transformer.py W4662FM0507 W4633080200 300 64 468 32 1
+python csc_transformer.py W4662FM0507 W4662FM0400 300 64 468 32 1
+python csc_transformer.py W4662FM0507 W4662FM0606 300 64 468 32 1
 
 # python csc_transformer.py W4662FM0507 W4662FM0605 100 2 468 32 0
 # python csc_transformer.py W4662FM0507 W4633070102 100 2 468 32 0
