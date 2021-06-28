0fbcdf13fe58a4588d5b19589378f7d73f1a787f
owen198 <cfleu198@gmail.com>
2021-06-28 18:41:12 +0800

    add

--- csc_transformer.py
+++ csc_transformer.py
@@ -481,7 +481,7 @@ rq2_cv_rmse = mean_squared_error (source_score_cv[-N:], source_score[-N:], squar
 record_pd = pd.read_csv('csc_record.csv')
 os.remove('csc_record.csv')
 record_list = [source, target, epoch, timesteps, units_layer_1, units_layer_2, rq1_rmse, rq2_rmse]
-record_pd.iloc[len(record_pd)] = record_list
+record_pd.append(pd.DataFrame(record_list), ignore_index = True)
 record_pd.to_csv('csc_record.csv')
 
 # rq2
