1899aafeea25b21390a50099782a1285f3c4e043
owen198 <cfleu198@gmail.com>
2021-07-02 12:26:33 +0800

    add

--- csc_transformer.py
+++ csc_transformer.py
@@ -62,7 +62,7 @@ else:
     #execution_list = execution_list.append('running')
     print(type(execution_list))
     print(execution_list)
-    print(execution_list.append('running'))
+    print(execution_list.append("running"))
     record_pd.loc[len(record_pd)] = execution_list.append('running')
     record_pd.to_csv('csc_execute.csv', mode='w+', index=False)
     execute_status = 'done'
