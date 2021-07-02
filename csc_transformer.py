d9c7c5bd57c2b106385654a9c239e0c0c704f9a0
owen198 <cfleu198@gmail.com>
2021-07-02 12:29:53 +0800

    add

--- csc_transformer.py
+++ csc_transformer.py
@@ -59,12 +59,9 @@ if len(record_pd[record_pd.drop(['status'], axis=1).isin(execution_list).all(axi
     logging.info(source+'_'+target+'_'+'task already executed')
     exit()
 else:
-    #execution_list = execution_list.append('running')
-    print(type(execution_list))
-    print(execution_list)
+
     execution_list = execution_list + ['running']
-    print(execution_list)
-    record_pd.loc[len(record_pd)] = execution_list.append('running')
+    record_pd.loc[len(record_pd)] = execution_list
     record_pd.to_csv('csc_execute.csv', mode='w+', index=False)
     execute_status = 'done'
 
